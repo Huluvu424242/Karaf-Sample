6dd3475613fc22d8543e7357c2644d528946842d
Thomas Schubert <funthomas424242@gmail.com>
2014-10-22 23:06:49 +0200

    fetch of kategorien fixed
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

--- ube-web/src/main/java/com/github/funthomas424242/ube/web/ShowCategoryServlet.java
+++ ube-web/src/main/java/com/github/funthomas424242/ube/web/ShowCategoryServlet.java
@@ -1,6 +1,5 @@
 package com.github.funthomas424242.ube.web;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -14,12 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.ops4j.pax.cdi.api.OsgiService;
 
 import de.inovex.javamagazin.domain.InventoryCategory;
-import de.inovex.javamagazin.domain.InventoryItem;
 import de.inovex.javamagazin.jpa.InventoryRepository;
 
 @WebServlet(urlPatterns = "/kategorien.json")
@@ -39,67 +35,47 @@ public class ShowCategoryServlet extends HttpServlet {
 			final HttpServletResponse response) throws ServletException,
 			IOException {
 
-//		// Request als JSON Object auslesen
-//		final StringBuffer sb = new StringBuffer();
-//		try {
-//			final BufferedReader reader = request.getReader();
-//			String line = null;
-//			while ((line = reader.readLine()) != null) {
-//				sb.append(line);
-//			}
-//		} catch (final Exception e) {
-//			e.printStackTrace();
-//		}
-//		final JSONParser parser = new JSONParser();
-//		JSONObject joUser = null;
-//		try {
-//			joUser = (JSONObject) parser.parse(sb.toString());
-//		} catch (final ParseException e) {
-//			e.printStackTrace();
-//		}
+		// // Request als JSON Object auslesen
+		// final StringBuffer sb = new StringBuffer();
+		// try {
+		// final BufferedReader reader = request.getReader();
+		// String line = null;
+		// while ((line = reader.readLine()) != null) {
+		// sb.append(line);
+		// }
+		// } catch (final Exception e) {
+		// e.printStackTrace();
+		// }
+		// final JSONParser parser = new JSONParser();
+		// JSONObject joUser = null;
+		// try {
+		// joUser = (JSONObject) parser.parse(sb.toString());
+		// } catch (final ParseException e) {
+		// e.printStackTrace();
+		// }
 
 		response.setCharacterEncoding("utf8");
 		response.setContentType("application/json");
 		final PrintWriter writer = response.getWriter();
-		
-		final JSONObject obj = new JSONObject();
-		obj.put("id", 3);
-		obj.put("name","horst");
-		obj.put("description", "blauer");
-		final ArrayList list=new ArrayList();
-		list.add(obj);
-		writer.print(list);
+		writer.print(getContent());
 		writer.flush();
 		writer.close();
 	}
 
-	private String getContent() {
+	private ArrayList<JSONObject> getContent() {
 
 		final List<InventoryCategory> allItems = inventoryRepository
 				.getAllCategories();
 
-		final StringBuffer out = new StringBuffer();
-		// out.append("liste=[");
-		out.append("[");
+		final ArrayList<JSONObject> kategorien = new ArrayList<JSONObject>();
 
-		boolean firstRun = true;
-		// array
-		for (InventoryCategory inventoryItem : allItems) {
-			if (!firstRun) {
-				out.append(",");
-			}
-			out.append("{id:'");
-			out.append(inventoryItem.getId());
-			out.append("',name:'");
-			out.append(inventoryItem.getCategoryName());
-			out.append("',description:'");
-			out.append(inventoryItem.getCategoryDescription());
-			out.append("'}");
-			firstRun = false;
+		for (InventoryCategory item : allItems) {
+			final JSONObject obj = new JSONObject();
+			obj.put("id", item.getId());
+			obj.put("name", item.getCategoryName());
+			obj.put("description", item.getCategoryDescription());
+			kategorien.add(obj);
 		}
-		out.append("]");
-
-		return out.toString();
-
+		return kategorien;
 	}
 }
