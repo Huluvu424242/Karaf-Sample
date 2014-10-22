5e5a5988312e058a46857432f65542acb9113aa0
Thomas Schubert <funthomas424242@gmail.com>
2014-10-22 23:11:14 +0200

    first working angularjs example for categories
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

--- ube-web/src/main/webapp/js/ContentController.js
+++ ube-web/src/main/webapp/js/ContentController.js
@@ -1,11 +1,15 @@
 'use strict';
 // definieren eines Moduls
 var contentModule = angular.module("contentModule", []);
- 
-// hinzufügen eines Controllers zum Modul
-contentModule.controller("ContentController", function ($scope, $http) {
 
-	$http.post('http://localhost:8181/ube/kategorien.json').success(function(data) {
-          $scope.liste=data;
-    });
+// hinzufügen eines Controllers zum Modul
+contentModule.controller("ContentController", function($scope, $http) {
+	
+	$scope.listCategories = function() {
+		
+		$http.post('http://localhost:8181/ube/kategorien.json').success(
+				function(data) {
+					$scope.liste = data;
+				});
+	}
 });
